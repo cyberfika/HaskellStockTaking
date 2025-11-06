b34bd826c5568b338eccb2d5aa08416be2dca0eb
Renato Gouveia <rpgouveia@gmail.com>
2025-11-06 01:37:43 -0300

    feat: update InventarioTipos, add Item type

--- InventarioTipos.hs
+++ InventarioTipos.hs
@@ -1,3 +1,5 @@
+-- Módulo: InventarioTipos.hs
+-- Descrição: Define os tipos de dados do sistema de inventário
 module InventarioTipos
     ( Item(..)                      
     , Inventario
@@ -9,14 +11,29 @@ module InventarioTipos
     , exemploLog
     ) where
 
+-- Importações
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Time (UTCTime)
 
 
+-- Tipo: Item
+-- Descrição: Representa um item no inventário
 data Item = Item
     { itemID :: String
     , nome :: String
     , quantidade :: Int
     , categoria :: String
     } deriving (Show, Read, Eq)
+
+
+-- Tipo: Inventario
+-- Descrição: Representa o inventário como um mapa de itens
+-- Implementação: Map usando itemID como chave e Item como valor
+type Inventario = Map String Item
+
+-- Função auxiliar para criar um inventário vazio
+emptyInventario :: Inventario
+emptyInventario = Map.empty
+
+
